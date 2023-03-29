820cecf768423db7b040353670a2930a9dc0b0f5
aldiansyah <aldiansyahrpl2@gmail.com>
2023-03-29 22:34:59 +0700

    memo maintenance

--- app/Model/InternalMemoBarang.php
+++ app/Model/InternalMemoBarang.php
@@ -9,4 +9,9 @@ class InternalMemoBarang extends Model
     protected $table = 'internal_memo_barang';
     protected $primaryKey = 'id';
     protected $guarded = [''];
+
+    function internalMemoBarang()
+    {
+        return $this->hasMany('App\Model\InternalMemoBarang', 'id_internal_memo', 'id');
+    }
 }
