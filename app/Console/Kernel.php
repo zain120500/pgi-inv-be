0862183cddf41721ceea6f415e3633cb7787afa9
aldiansyah <aldiansyahrpl2@gmail.com>
2023-05-14 20:47:37 +0700

    create barang maintenance

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('test:cron')->daily();
+        $schedule->command('test:cron')->hourlyAt(00);
     }
 
     /**
@@ -35,7 +35,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
